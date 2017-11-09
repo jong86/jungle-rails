cf65eefd1830d33a36f5b168517cd3ab8d966b92
Jon Gaspar <jon_gaspar@hotmail.com>
2017-11-09 03:08:03 +0000

    Created category routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,9 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :products, :categories, except: [:edit, :update, :show, :destroy]
   end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
