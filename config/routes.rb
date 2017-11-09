add14e6b4056f42f3f45f2e373b18c909738fdb6
Jon Gaspar <jon_gaspar@hotmail.com>
2017-11-09 18:23:54 +0000

    Created initial user login/register/logout models, controllers, views, routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,15 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
+  namespace :users do
+    get :login
+    post :login
+    get :register
+    post :register
+    post :logout
+  end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625062916) do
+ActiveRecord::Schema.define(version: 20171109174425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,14 @@ ActiveRecord::Schema.define(version: 20160625062916) do
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
