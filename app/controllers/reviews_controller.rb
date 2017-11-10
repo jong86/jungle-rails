e8bcad5805ea003bd07dfb53f70e062239c468de
Jon Gaspar <jon_gaspar@hotmail.com>
2017-11-10 23:50:51 +0000

    Delete button for reviews, reviews table hides if there is no reviews, review form only shows if user logged in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_filter :authorize
+
   def create
     # raise "Yay, Im here!"
     review = Review.new(review_params)
@@ -6,19 +8,17 @@ class ReviewsController < ApplicationController
     review.user_id = current_user.id
     review.product_id = params[:product_id]
 
-    product = Product.find(params[:product_id])
-
     if review.save
-      # Review.create(
-      #   product_id: review.product_id,
-      #   user_id: review.user_id,
-      #   description: review.description,
-      #   rating: review.rating
-      # )
       redirect_to "/products/#{review.product_id}"
     else
       redirect_to "/products/#{review.product_id}"
     end
+  end
+
+  def destroy
+    # byebug
+    Review.find(params[:review_id]).destroy
+    redirect_to "/products/#{params[:id]}"
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :products, only: [:index, :show] do
-    resources :reviews, only: [:create]
+  resources :products, only: [:index, :show], shallow: true do
+    resources :reviews, only: [:create, :destroy]
   end
 
   resources :categories, only: [:show]
