974d862d378451e8ccd4f6f5c191b1c7df2cd5cc
Jon Gaspar <jon_gaspar@hotmail.com>
2017-11-11 21:09:39 +0000

    Added order id to order email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,11 +10,12 @@ class UserMailer < ApplicationMailer
       @products.push(Product.where(product_id: item[:product_id]))
     end
 
-    mail(to: order.email, subject: 'Your Jungle order')
+    mail(to: order.email, subject: "Your receipt for Jungle order ##{order.id}")
   end
 
   def welcome_email(user) # sent upon user registration
     @user = user
     mail(to: user.email, subject: "Welcome to Jungle #{user.first_name}!")
   end
+
 end
