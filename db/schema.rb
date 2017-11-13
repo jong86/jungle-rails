f7a1ef519ab21c515ec78ff3d6e021b4985cba46
Jon Gaspar <jon_gaspar@hotmail.com>
2017-11-13 18:50:22 +0000

    Added rspec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111195626) do
+ActiveRecord::Schema.define(version: 20171110165344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,8 +81,7 @@ ActiveRecord::Schema.define(version: 20171111195626) do
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "name"
   end
 
   add_foreign_key "line_items", "orders"
