b9c697bed2974f1370ae8c320364157da85f8c57
Jon Gaspar <jon_gaspar@hotmail.com>
2017-11-13 20:13:22 +0000

    Added 5 validations for product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    it "is saveable" do
+      @category = Category.create(name: 'Stuff')
+      @product = @category.products.create({
+        name:  'Red Bookshelf',
+        description: 'awesome thing',
+        image: 'bookshelf.jpg',
+        quantity: 23,
+        price: 2_483.75
+      })
+      expect(@product).to be_a Product
+    end
+
+    it "has a valid name" do
+      @category = Category.create(name: 'Stuff')
+      @product = @category.products.create({
+        name: nil,
+        description: 'awesome thing',
+        image: 'bookshelf.jpg',
+        quantity: 23,
+        price: 2_483.75
+      })
+      expect(@product.errors[:name]).to include("can't be blank")
+    end
+
+    it "has a valid price" do
+      @category = Category.create(name: 'Stuff')
+      @product = @category.products.create({
+        name:  'Red Bookshelf',
+        description: 'awesome thing',
+        image: 'bookshelf.jpg',
+        quantity: 23,
+        price: nil
+      })
+      expect(@product.errors[:price]).to include("can't be blank")
+    end
+
+    it "has a valid quantity" do
+      @category = Category.create(name: 'Stuff')
+      @product = @category.products.create({
+        name:  'Red Bookshelf',
+        description: 'awesome thing',
+        image: 'bookshelf.jpg',
+        quantity: nil,
+        price: 2_483.75
+      })
+      expect(@product.errors[:quantity]).to include("can't be blank")
+    end
+
+    it "has a valid category" do
+      # @category = Category.create(name: 'Stuff')
+      @product = Product.create({
+        name:  'Red Bookshelf',
+        description: 'awesome thing',
+        image: 'bookshelf.jpg',
+        quantity: 23,
+        price: 2_483.75
+      })
+      expect(@product.errors[:category]).to include("can't be blank")
+    end
+
+  end
+
+end
